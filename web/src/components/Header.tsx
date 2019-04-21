42b70c953a319f7702d3a740fe9a3944204d25b1
Ivan Butrim <vbutrim@gmail.com>
2019-04-21 18:12:48 +0300

    terms of use page

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -8,6 +8,7 @@ export const Header = () => (
             <ul>
                 <li><Link to={'/'}>Home</Link></li>
                 <li><Link to={'/policy'}>Policy</Link></li>
+                <li><Link to={'/rules'}>Terms of Use</Link></li>
             </ul>
         </nav>
     </header>
--- web/src/components/Main.tsx
+++ web/src/components/Main.tsx
@@ -3,6 +3,7 @@ import {Route, Switch} from 'react-router';
 import ErrorNotFound from './ErrorNotFound';
 import MenuContainer from './MenuContainer';
 import Policy from './Policy';
+import TermsOfUse from './TermsOfUse';
 
 
 export const Main = () => (
@@ -10,6 +11,7 @@ export const Main = () => (
         <Switch>
             <Route exact path="/" component={MenuContainer}/>
             <Route exact path="/policy" component={Policy}/>
+            <Route exact path="/rules" component={TermsOfUse}/>
             <Route path="*" component={ErrorNotFound}/>
         </Switch>
     </main>
