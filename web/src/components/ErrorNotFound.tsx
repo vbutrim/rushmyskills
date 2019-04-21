4b42ab961ee55bd6a0b1c9f7e12200dbbd6ae6ca
Ivan Butrim <vbutrim@gmail.com>
2019-04-21 16:44:58 +0300

    styled error page

--- web/src/components/ErrorNotFound.tsx
+++ web/src/components/ErrorNotFound.tsx
@@ -4,13 +4,20 @@ import {EmptyProps} from '../common/EmptyProps';
 
 const Div = styled.div`
   margin: 40px;
+  text-align: center;
 `;
 
 class ErrorNotFound extends React.Component<EmptyProps> {
     public render(): React.ReactNode {
         return (
             <Div>
-                aviv olds
+                <div id="error">
+                    <h1 className="notFoundTitle">Oops! That page can’t be found.</h1>
+                    <p className="notFoundDesc">
+                        It looks like nothing was found at this location.
+                        Maybe try one of the links in the menu or press back to go to the previous page.
+                    </p>
+                </div>
             </Div>
         );
     }
